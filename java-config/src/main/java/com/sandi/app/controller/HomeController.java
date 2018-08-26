ff9cb534c1cad8fd17707950144747642b973293
sandeeptiwari <sandeep15mca@gmail.com>
2018-08-26 22:50:36 +0530

    @PathVariable example

--- java-config/src/main/java/com/sandi/app/controller/HomeController.java
+++ java-config/src/main/java/com/sandi/app/controller/HomeController.java
@@ -1,18 +1,36 @@
 package com.sandi.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
 
+//echo "" >.gitignore
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(ModelMap model){
         model.addAttribute("message", "Spring MVC Java Config Example");
         return "index";
     }
 
+    @RequestMapping("/welcome")
+    public ModelAndView helloWorld(){
+        ModelAndView  model = new ModelAndView("HelloPage");
+        model.addObject("msg", "hello world");
+
+        return model;
+    }
+
+    @RequestMapping(value="/welcome/{countryName}/{userName}", method = RequestMethod.GET)
+    public ModelAndView userName(@PathVariable String countryName, @PathVariable String userName){
+        ModelAndView  model = new ModelAndView("HelloPage");
+        model.addObject("country", countryName);
+        model.addObject("name", userName);
+
+        return model;
+    }
 }
---
+++ java-config/src/main/webapp/WEB-INF/views/HelloPage.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Sandeep
+  Date: 26-08-2018
+  Time: 10:37 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+ ${msg}
+
+ ${country}
+ ${name}
+</body>
+</html>
